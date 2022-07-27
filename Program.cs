1fd83327b2039993bd9573c48dc7454be156c77a
Almir <crafteasy@yandex.ru>
2022-07-27 20:41:07 +0300

    Написал метод нахождения размера для нового массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayOld = new string[] { "fwer", "cat", "gerg", "fsfg", "ff" };
+
+int ArrayFindWords3(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
